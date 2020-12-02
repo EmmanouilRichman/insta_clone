9642e601d33e96243ef2842eed27176d1cac9950
Emmanouil Richman <erichman966@gmail.com>
2020-12-02 18:38:59 -0500

    Finished Backend for Posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -26,4 +26,33 @@ router.route('/add').post((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => res.json(post))
+        .catch(err => res.status(400).json('Error: ' + err))
+});
+
+router.route('/:id').delete((req, res) => {
+    Post.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Post Deleted'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/update/:id').post((req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            post.title = req.body.title;
+            post.description = req.body.description;
+            post.img = req.body.image;
+            post.date = req.body.date;
+            post.likeCount = req.body.likeCount;
+            post.likes = req.body.likes;
+
+            post.save()
+                .then(() => res.json('Post Updated'))
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
+        .catch(err => res.status(400).json('Error ' + err))
+});
+
 module.exports = router;
